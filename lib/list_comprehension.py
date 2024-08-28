a92950e382cf3f634c0931576c668bb068506e3f
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-28 16:23:09 +0300

    complete tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -7,10 +7,6 @@ def return_evens(num_list):
 print(return_evens([0, 1, 3, 5, 7, 8, 9]))    
 
 def make_exclamation(sentence_list):
-    # add an ! to a character if !. are missing
-    # else if !. are present return the character as they are
-    new_sentence = [char + '!' if char not in '!.' else char for char in sentence_list]
-    return new_sentence
+    return [sentence + "!" for sentence in sentence_list]  
 
-print(make_exclamation("hell'o! my name is Bree."))
-
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
